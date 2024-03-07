cfef4932bfd919d87f600d0ec5c5b130b1c1e913
edgarmsilva <edgar.msilva@gmail.com>
2024-03-07 11:22:45 -0300

    Update run-tests.sh

--- run-tests.sh
+++ run-tests.sh
@@ -12,12 +12,6 @@ export K6_BROWSER_HEADLESS=true
 export K6_BROWSER_ARGS='no-sandbox' 
 export K6_BROWSER_EXECUTABLE_PATH=/usr/bin/google-chrome
 
-# for test in $TESTS; do
-# 	# Disable thresholds because some threshold examples fail
-# 	k6 run --no-thresholds -e BASE_URL=$BASE_URL "$test"
+k6 run scripts/web2.js
 
-# 	exit_code=$?
-# 	if [ $exit_code -ne 0 ]; then
-# 		exit $exit_code
-# 	fi
-# done
+
